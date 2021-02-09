0246c65f8ddfcaa07cb8d44ceaa67be8005dad2d
Katie Ballentine <knees4bees@gmail.com>
2021-02-09 15:25:24 -0700

    Build constructor

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,5 +1,12 @@
 class Recipe {
-
+  constructor(recipe) {
+    this.id = recipe.id;
+    this.image = recipe.image;
+    this.ingredients = recipe.ingredients;
+    this.instructions = recipe.instructions;
+    this.name = recipe.name;
+    this.tags = recipe.tags;
+  }
 }
 
 module.exports = Recipe;
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,7 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { testRecipeData, testUsersData, testIngredientsData } = require('../data/test-data');
+const { 
+  testRecipeData, 
+  testUsersData, 
+  testIngredientsData 
+} = require('../data/test-data');
 
 const Recipe = require('../src/Recipe');
 
@@ -9,14 +13,7 @@ describe('Recipe', function() {
   let recipe;
 
   beforeEach(function() {
-    recipe = new Recipe(
-      testRecipeData[0].id, 
-      testRecipeData[0].image, 
-      testRecipeData[0].ingredients, 
-      testRecipeData[0].instructions, 
-      testRecipeData[0].name, 
-      testRecipeData[0].tags, 
-    );
+    recipe = new Recipe(testRecipeData[0]);
   });
 
   it('should be a function', function() {
@@ -27,29 +24,27 @@ describe('Recipe', function() {
     expect(recipe).to.be.an.instanceof(Recipe);
   }); 
 
-  // TODO get this to run -- issue with test export/import?
   it('should have an id', function() {
-    console.log(recipe.id);
     expect(recipe.id).to.equal(testRecipeData[0].id);
   });  
 
-  it.skip('should have an image', function() {
+  it('should have an image', function() {
     expect(recipe.image).to.equal(testRecipeData[0].image);
   });  
 
-  it.skip('should have ingredients', function() {
+  it('should have ingredients', function() {
     expect(recipe.ingredients).to.deep.equal(testRecipeData[0].ingredients);
   });
 
-  it.skip('should have instructions', function() {
+  it('should have instructions', function() {
     expect(recipe.instructions).to.deep.equal(testRecipeData[0].instructions);
   });
 
-  it.skip('should have a name', function() {
+  it('should have a name', function() {
     expect(recipe.name).to.equal(testRecipeData[0].name);
   });
 
-  it.skip('should have tags', function() {
+  it('should have tags', function() {
     expect(recipe.tags).to.deep.equal(testRecipeData[0].tags);
   });
 });
