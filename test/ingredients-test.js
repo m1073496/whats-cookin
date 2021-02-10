f1a806c1288d59860350deddad6ff33bc6d99f95
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-02-09 17:44:04 -0700

    Write tests for skeleton Ingredient class

--- test/ingredients-test.js
+++ test/ingredients-test.js
@@ -3,7 +3,17 @@ const expect = chai.expect;
 
 const Ingredient = require('../src/Ingredient');
 
-describe(Ingredient, function(){
+describe('Ingredient', function() {
+
+  it('should be a function', function() {
+    const ingredient = new Ingredient();
+    expect(ingredient).to.be.a('function');
+  });
+
+  it('should create an instance of an Ingredient', function() {
+    const ingredient = new Ingredient();
+    expect(ingredient).to.be.an.instanceOf(Ingredient);
+  });
 
 
 
