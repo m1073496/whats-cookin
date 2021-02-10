11315221d06405e3b7441fe7fa4b59d0e46b8166
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-02-09 17:55:06 -0700

    Write tests for Ingredients properties

--- test/ingredients-test.js
+++ test/ingredients-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const { testIngredientsData } = require('./data/test-data.js');
 const Ingredient = require('../src/Ingredient');
 
 describe('Ingredient', function() {
@@ -13,6 +14,21 @@ describe('Ingredient', function() {
   it('should create an instance of an Ingredient', function() {
     const ingredient = new Ingredient();
     expect(ingredient).to.be.an.instanceOf(Ingredient);
+  });
+
+  it('should have an ID', function() {
+    const ingredient = new Ingredient(testIngredientsData[0]);
+    expect(ingredient.id).to.equal(9019);
+  });
+
+  it('should have a name', function() {
+    const ingredient = new Ingredient(testIngredientsData[1]);
+    expect(ingredient.name).to.equal('dry breadcrumbs');
+  });
+
+  it('should have an estimated cost in cents', function() {
+    const ingredient = new Ingredient(testIngredientsData[2]);
+    expect(ingredient.estimatedCostInCents).to.equal(903);
   });
 
 
