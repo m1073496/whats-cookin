2d7a8d80e32eeb370b9053b9cbf53fbd4b64aa91
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-02-11 08:10:32 -0700

    Add functionality to All Recipes nav button

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,19 @@
-console.log('Hello world');
+var allRecipesButton = document.getElementById('all-recipes');
+var landingView = document.querySelector('.landing-view');
+var recipeDetailView = document.querySelector('.recipe-detail-view');
+var featuredSections = document.querySelector('.featured-section');
+var recipeDetailLeft = document.querySelector('.recipe-detail__bottom--left');
+var recipeDetailRight = document.querySelector('.recipe-detail__bottom--right');
+
+
+
+allRecipesButton.addEventListener('click', displayRecipeList);
+
+function displayRecipeList() {
+  landingView.classList.add('hidden');
+  featuredSections.classList.add('hidden');
+
+  recipeDetailView.classList.remove('hidden');
+  recipeDetailLeft.classList.remove('hidden');
+  recipeDetailRight.classList.remove('hidden');
+}
