ba18e079316bdfab28312c2659c814f9d237b229
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-02-11 15:21:21 -0700

    Hide landing view, show recipe list view on click

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,12 @@
 const allRecipesButton = document.getElementById('all-recipes');
 const landingView = document.querySelector('.landing-view');
 // const recipeDetailView = document.querySelector('.recipe-detail-view');
-const featuredSections = document.querySelector('.featured-section');
+const featuredSection1 = document.querySelector('.featured-content1');
+const featuredSection2 = document.querySelector('.featured-content2');
 const recipeListView = document.querySelector('.list-view');
-const recipeListLeft = document.querySelector('.recipe-list__item');
-// const recipeListRight = document.querySelector('.recipe-list__item content2');
+const recipeListContent1 = document.querySelector('.recipe-list-content1');
+const recipeListContent2 = document.querySelector('.recipe-list-content2');
+
 
 
 hide = (element) => element.classList.add('hidden');
@@ -12,13 +14,13 @@ hide = (element) => element.classList.add('hidden');
 display = (element) => element.classList.remove('hidden');
 
 displayRecipeList = () => {
-  console.log('This is working')
   hide(landingView);
-  hide(featuredSections);
+  hide(featuredSection1);
+  hide(featuredSection2);
 
   display(recipeListView);
-  display(recipeListLeft);
-  // display(recipeListRight);
+  display(recipeListContent1);
+  display(recipeListContent2);
 }
 
 allRecipesButton.addEventListener('click', displayRecipeList);
