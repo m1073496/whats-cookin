454039394c9fe01bc57192cbc7dceefdd13145c5
Nikki <piknik@me.com>
2021-02-11 06:49:36 -0700

    happy and sad path for tag filter

--- src/RecipeRepository.js
+++ src/RecipeRepository.js
@@ -1,6 +1,32 @@
-class RecipeRepository {
-  constructor() {
 
+const { ingredientsData } = require('../data/ingredients');
+
+class RecipeRepository {
+  constructor(data) {
+    this.recipes = data
+  }
+
+  filterByTag(searchTerm) {
+    return this.recipes.filter(recipe => {
+      if (recipe.tags.includes(searchTerm)) {
+        return recipe.id;
+      }
+    })
+  }
+
+  filterByIngredient(searchTerm) {
+    // todo ==> this isn't working yet.
+    return this.recipes.filter(recipe => {
+      return recipe.ingredients.id.includes(searchTerm)
+    })
+  }
+
+  getIngredientName(id) {
+    const match = ingredientsData.find(ingredient => {
+      return ingredient.id === id;
+    });
+
+    return match.name;
   }
 }
 
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,20 +1,35 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { RecipeData } = require('../data/recipes');
+const { testRecipeData } = require('../data/test-data');
 const RecipeRepository = require('../src/RecipeRepository');
 
 describe('RecipeRepository', () => {
-
   let recipeRepository;
 
   beforeEach(() => {
-    recipeRepository = new RecipeRepository()
-  })
+    recipeRepository = new RecipeRepository(testRecipeData)
+  });
 
   it('should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
     expect(recipeRepository).to.exist;
-  })
+  });
 
-})
+  it('should filter tags using a search term', () => {
+    expect(recipeRepository.filterByTag('side dish').length).to.equal(2)
+  });
+
+  it('should return message when tag filter returns no results', () => {
+    expect(recipeRepository.filterByTag('midnight snack').length).to.equal(0)
+  });
+
+  xit('should filter ingredients using a search term', () => {
+    expect(recipeRepository.filterByIngredient(11282).length).to.equal(3)
+  });
+
+  xit('should return message when ingredient filter returns no results', () => {
+    expect(recipeRepository.filterByIngredient(11111).length).to.equal(3)
+  });
+
+});
