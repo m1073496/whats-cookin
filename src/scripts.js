e98128dfc5c78c86249591efaa6d03b48bfcdea0
Katie Bee <knees4bees@gmail.com>
2021-02-12 08:01:53 -0700

    Draft in progress of search listener

--- src/scripts.js
+++ src/scripts.js
@@ -25,9 +25,35 @@ allRecipesButton.addEventListener('click', displayRecipeList);
 
 
 
-const searchBar = document.querySelector('.search-bar');
+const searchBarInput = document.querySelector('.search-bar');
 const searchButton = document.querySelector('.search-button');
 
 
 
-searchButton.addEventListener('click', search);
+searchButton.addEventListener('click', function() {
+  search(searchBarInput);
+});
+
+function search(input) {
+  const words = formatInput(input);
+
+  // TODO: need to import RecipeRepository? Assign const allRecipes = recipeRepository.recipes?
+  // for each word, run filter by ingredient (recipe repository method)
+    // (will return array of recipe objects for each ingredient)
+  const foundRecipes = words.map(word => {
+    return allRecipes.filterByIngredient(word);
+  });
+
+
+
+  // use array.flat() to flatten the result into a single array of recipe objects
+    // (the flat method will remove any empty arrays if any word mapped to an empty array b/c no results)
+  // remove duplicates
+  // if array has anything in it,
+    // return resulting array of unique recipe objects
+    // else return error message saying the search got no results
+}
+
+function formatInput(input) {
+  return input.toLowerCase().split(' ');
+}
