2aa5e00465abedf85f1fcd0625a68a11327dd86a
Katie Bee <knees4bees@gmail.com>
2021-02-17 20:37:08 -0700

    Remove broken cookMeal method and test

--- src/User.js
+++ src/User.js
@@ -54,48 +54,6 @@ class User {
     });
     return missingIngredients;
   }
-
-  cookMeal = (recipe) => {
-    // console.log(this.findMissingIngredients(recipe))
-    if (this.findMissingIngredients(recipe).length === 0) {
-      // let ingredientsToRemoveFromPantry = recipe.ingredients.map(ingredient => {
-      //   return this.userPantry.find(element => element.ingredient === ingredient.id);
-      // });
-      // console.log("ings to remove: ", ingredientsToRemoveFromPantry);
-
-      recipe.ingredients.forEach(ingredient => {
-        const recipeAmount = ingredient.amount;
-        const pantryAmount = this.userPantry.find(element => element.ingredient === ingredient.id).amount;
-        const extraAmount = pantryAmount - recipeAmount;
-
-        const match = this.userPantry.find(element => element.ingredient === ingredient.id);
-
-        if (recipeAmount === pantryAmount) {
-          this.userPantry.splice(match);
-        } else {
-          this.userPantry.find(element => element.ingredient === ingredient.id).amount = extraAmount;
-        }
-      });
-      // if the recipeAmount === pantryAmount, splice pantry ingredient entirely
-      // else if the pantryAmount > recipeAmount, subtract recipeAmount from pantryAmount in pantry
-
-      // let ingredientIndexes = ingredientsToRemoveFromPantry.map(item => {
-      //   ingredientsToRemoveFromPantry.indexOf(item);
-      // });
-      // console.log(ingredientIndexes);
-
-      // console.log(this.userName, this.userPantry)
-      // this.userPantry.forEach(item => {
-      //   // this.userPantry.splice(ingredientsToRemoveFromPantry.forEach(ingredient => ingredient[ingredientIndexes]))
-      //   this.userPantry.splice(ingredientsToRemoveFromPantry);
-      //   // forEach(element => element[[1, 4, 5]]);
-      // });
-      // console.log(this.userName, this.userPantry)
-
-      // let recipeToRemove = this.recipesToCook.indexOf(recipe);
-      //   this.recipesToCook.splice(recipeToRemove, 1);
-    }
-  };
 };
 
 if (typeof module !== 'undefined') {
